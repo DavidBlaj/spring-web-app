238913a5dd6342c6dd6f6de03da7382e838ae79c
David <blaj.david1@gmail.com>
2023-10-22 11:30:00 +0200

    1.0.3 - Added relation of Many Publishers to one Book and add H2 console

--- src/main/java/com/springtutorial/springwebapp/bootstrap/BootstrapData.java
+++ src/main/java/com/springtutorial/springwebapp/bootstrap/BootstrapData.java
@@ -9,8 +9,12 @@ import com.springtutorial.springwebapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+
 @Component
 public class BootstrapData implements CommandLineRunner {
+
+    Scanner scanner = new Scanner(System.in);
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
@@ -24,33 +28,29 @@ public class BootstrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Author eric = new Author();
-        eric.setFirstName("Eric");
-        eric.setLastName("some lastname");
-
-        Book book = new Book();
-        book.setTitle("Some title");
-        book.setIsbn("12345678");
-
-
-        Author ericSaved = authorRepository.save(eric);
-        Book bookSaved = bookRepository.save(book);
-
 
         Author rod = new Author();
-        eric.setFirstName("Rod");
-        eric.setLastName("Johnson");
+        rod.setFirstName("Rodwqd");
+        rod.setLastName("Johnson");
 
         Book someBook = new Book();
-        book.setTitle("Some title for the second book");
-        book.setIsbn("123456781");
+        someBook.setTitle("Some title for the first book");
+        someBook.setIsbn("123456781");
 
+        Book someBook2 = new Book();
+        someBook2.setTitle("Some title for the second book");
+        someBook2.setIsbn("123456781");
 
         Author rodSaved = authorRepository.save(rod);
         Book someBookSaved = bookRepository.save(someBook);
+        Book someBookSaved2 = bookRepository.save(someBook2);
 
-        ericSaved.getBooks().add(bookSaved);
+
         rodSaved.getBooks().add(someBookSaved);
+        rodSaved.getBooks().add(someBookSaved2);
+
+        someBookSaved.getAuthors().add(rodSaved);
+        someBookSaved2.getAuthors().add(rodSaved);
 
         Publisher amazonBooks = new Publisher();
         amazonBooks.setPublisherName("Amazon Books");
@@ -61,15 +61,13 @@ public class BootstrapData implements CommandLineRunner {
 
         Publisher amazonBooksSaved = publisherRepository.save(amazonBooks);
 
-        bookSaved.setPublisher(amazonBooksSaved);
-        book.setPublisher(amazonBooksSaved);
+        someBookSaved.setPublisher(amazonBooksSaved);
+        someBook.setPublisher(amazonBooksSaved);
+        someBook2.setPublisher(amazonBooksSaved);
 
-        authorRepository.save(ericSaved);
+        bookRepository.save(someBook);
+        bookRepository.save(someBook2);
+
         authorRepository.save(rodSaved);
-        bookRepository.save(bookSaved);
-        bookRepository.save(book);
-
-        System.out.println(publisherRepository.count());
-
     }
 }
